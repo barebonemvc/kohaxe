bf2534fef26859bae73317bb0681e2f3b65bda19
Ovidiu <ovidiugabriel@gmail.com>
2021-04-22 15:20:19 +0300

    Varargs on var_dump

--- haxe/php/native/PhpNative.hx
+++ haxe/php/native/PhpNative.hx
@@ -2,5 +2,5 @@ package php.native;
 
 class PhpNative {
     @:extern @:native('var_dump')
-    static public function var_dump(value : Dynamic) : Void;
+    static public function var_dump(...value : Dynamic) : Void;
 } 
