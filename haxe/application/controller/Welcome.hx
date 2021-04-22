476520753fa9a6759339dc4028bc2892f8166f19
Ovidiu <ovidiugabriel@gmail.com>
2021-04-22 06:10:14 +0300

    Typed request and response

--- haxe/application/controller/Welcome.hx
+++ haxe/application/controller/Welcome.hx
@@ -2,10 +2,12 @@
 package controller;
 
 import kohana.Controller;
+import kohana.Request;
+import kohana.Response;
 
 @:native('Controller_Welcome')
 class Welcome extends Controller {
-    public function new(request : Dynamic, response : Dynamic) {
+    public function new(request : Request, response : Response) {
         super(request, response);
     }
 
