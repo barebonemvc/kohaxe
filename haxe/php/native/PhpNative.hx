775c1926940a1e20c1bdf4fe32502349459e34c4
Ovidiu <ovidiugabriel@gmail.com>
2021-04-23 16:06:29 +0300

    Update PhpNative.hx

--- haxe/php/native/PhpNative.hx
+++ haxe/php/native/PhpNative.hx
@@ -3,4 +3,10 @@ package php.native;
 class PhpNative {
     @:extern @:native('var_dump')
     static public function var_dump(...value : Dynamic) : Void;
+
+    @:extern @:native('print_r')
+    static public function print_r(value : Dynamic, returnIt : Bool) : Dynamic;
+
+    @:extern @:native('var_export')
+    static public function var_export(value : Dynamic, returnIt : Bool) : Dynamic;
 } 
