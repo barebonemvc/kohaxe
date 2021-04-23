e1aebb7398c4bbd85f1eb18a6f5370cf6b22d07c
Ovidiu Farauanu <ovidiugabriel@gmail.com>
2021-04-23 20:46:08 +0300

    Added this.request.body()

--- haxe/kohana/Controller.hx
+++ haxe/kohana/Controller.hx
@@ -3,5 +3,6 @@ package kohana;
 
 @:native('Kohana_Controller')
 extern class Controller {
+    var request : Request;
     public function new(request : Request, response : Response);
 }
--- haxe/kohana/Request.hx
+++ haxe/kohana/Request.hx
@@ -2,5 +2,5 @@ package kohana;
 
 @:native('Kohana_Request')
 extern class Request {
-
+    public function body() : String;
 }
